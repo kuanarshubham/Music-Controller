f8c9bb7660d9bd7ac6048afe18c2cda167eb3bed
Shubham <kuanarshubham@gmail.com>
2024-04-08 23:42:49 +0530

    Implemented the Model - Room & created the base of react frontend

--- api/models.py
+++ api/models.py
@@ -7,14 +7,14 @@ def generate_unique_code():
 
     while True:
         code = ''.join(random.choice(string.ascii_uppercase, k=lenght))
-        if Room.objects.filter(code=code).count() ==0:
+        if Room.objects.filter(code=code).count() == 0:
             break
         
     return code
 
 class Room(models.Model):
-    code = models.CharField(max_lenght=8, default="", unique=True)
+    code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    votes_to_skip  = models.BooleanField(null=False, default=1)
+    votes_to_skip  = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rest_framework import generics
+from .serializer import RoomSerializer
+from .models import Room
 
-def main(request):
-    return HttpResponse("what are you talking about")
+# def main(request):
+#     return HttpResponse("what are you talking about")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
